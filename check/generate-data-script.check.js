9dd08b3f78035523b46ca902d550f9d5c5268ca1
khai96_ <hvksmr1996@gmail.com>
2018-01-15 16:39:15 +0700

    Try this

--- check/generate-data-script.check.js
+++ check/generate-data-script.check.js
@@ -1,43 +1,40 @@
 'use strict'
 const path = require('path')
 const fs = require('fs-extra')
+const root = path.dirname(__dirname)
+const getpath = filename => path.resolve(root, filename)
 
-describe('Generate Data Script', () => {
-  const root = path.dirname(__dirname)
-  const getpath = filename => path.resolve(root, filename)
+describe('should create prime.static.min.js', () => {
+  const filename = getpath('prime.static.min.js')
 
-  describe('should create prime.static.min.js', () => {
-    const filename = getpath('prime.static.min.js')
-
-    it('that is a file', async () => {
-      expect((await fs.stat(filename)).isFile()).toBe(true)
-    })
-
-    it('that is a valid Node.js module', () => {
-      expect(require(filename)).toBeInstanceOf(Array)
-    })
+  it('that is a file', async () => {
+    expect((await fs.stat(filename)).isFile()).toBe(true)
   })
 
-  describe('should create prime.static.min.json', () => {
-    const filename = getpath('prime.static.min.json')
+  it('that is a valid Node.js module', () => {
+    expect(require(filename)).toBeInstanceOf(Array)
+  })
+})
 
-    it('that is a file', async () => {
-      expect((await fs.stat(filename)).isFile()).toBe(true)
-    })
+describe('should create prime.static.min.json', () => {
+  const filename = getpath('prime.static.min.json')
 
-    it('that is a valid JSON file', async () => {
-      const array = JSON.parse(await fs.readFile(filename, 'utf8'))
-      expect(array).toBeInstanceOf(Array)
-    })
+  it('that is a file', async () => {
+    expect((await fs.stat(filename)).isFile()).toBe(true)
   })
 
-  it('should create two equal module', async () => {
-    expect(
-      require(getpath('prime.static.min.js'))
-    ).toEqual(
-      JSON.parse(
-        await fs.readFile(getpath('prime.static.min.json'), 'utf8')
-      )
+  it('that is a valid JSON file', async () => {
+    const array = JSON.parse(await fs.readFile(filename, 'utf8'))
+    expect(array).toBeInstanceOf(Array)
+  })
+})
+
+it('should create two equal module', async () => {
+  expect(
+    require(getpath('prime.static.min.js'))
+  ).toEqual(
+    JSON.parse(
+      await fs.readFile(getpath('prime.static.min.json'), 'utf8')
     )
-  })
+  )
 })
